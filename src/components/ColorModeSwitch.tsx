bda9b9ef862934c75324c6c0bbcea7a7d15ab9f0
Arjun <6112arjun@gmail.com>
2023-06-24 17:15:37 +0530

    Built Search UI

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -10,7 +10,7 @@ const ColorModeSwitch = () => {
         onChange={toggleColorMode}
         id="theme-switch"
       />
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,13 @@ import { HStack, Image } from "@chakra-ui/react";
 
 import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="15px">
+    <HStack padding="15px">
       <Image src={logo} alt="logo" boxSize="60px" borderRadius="15px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
